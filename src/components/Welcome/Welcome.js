aadbe82d38b90493155d43ee2446dd53c2b92774
Princesharma03 <prince.sharma@innovaccer.com>
2022-05-01 18:06:51 +0530

    react push

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,41 @@
+import { Fragment } from 'react';
+import './style.css';
+import { useState } from 'react';
+
+const Welcome=()=>{
+    const [input,setInput]=useState("");
+  
+    const changeInput=(event)=>{
+        setInput(event.target.value);
+    }
+
+
+
+    return (
+        <Fragment>
+            <div className='welcomeScreen'>
+                <h1>Collaborative whiteboard</h1>
+            <h2>Get hands-on right away with the Web whiteboard for instant collaboration, where you can brainstorm, share ideas and manage projects without signing-up.</h2>
+            <form >
+
+            <label>
+                <h3>Create or join room </h3>
+                <p>Please enter code:</p>
+                </label>
+                <div className='blockDiv'> 
+                <input onChange={changeInput} className='inputCss' type="text" name="name" />
+                </div >
+
+            <input className='submitCss' onClick={()=>{
+                window.location.href = `https://evening-dusk-32043.herokuapp.com/whiteboard/${input}`;
+
+            }}  value="Create OR Join" />
+                
+            
+            </form>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Welcome;
---
+++ src/components/container/Container.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import Board from '../board/Board'
+import { useParams } from "react-router-dom";
+import { Clipboard2Plus } from "react-bootstrap-icons";
+
+import { useState } from 'react';
+import './style.css';
+// import io from 'socket.io-client';
+// const socket = io.connect("http://localhost:3000");
+
+
+const Container=()=>{
+    
+    const [color,setColor]=useState("#000000");
+    const [size,setSize]=useState("5");
+    const prams=useParams();
+    // socket.emit("canvas-data",prams.room);
+
+    const changeColor=(event)=>{
+        setColor(event.target.value);
+    }
+    const changeSize=(event)=>{
+        setSize(event.target.value);
+    }
+    
+    const myFunction=() => {
+        navigator.clipboard.writeText("https://evening-dusk-32043.herokuapp.com/whiteboard/"+prams.room);
+    }
+
+
+
+
+
+
+    return(
+        <div className='container'>
+                           <div className='color-picker-container'>
+                               Select Brush Color : &nbsp;
+                               <input type="color" value={color} onChange={changeColor} />
+                           </div>
+                           <div className='brushsize-container'>
+                               Select Brush size : &nbsp;
+                           <select value={size} onChange={setSize}> 
+                               <option>5</option>
+                               <option>10</option>
+                               <option>15</option>
+                               <option>20</option>
+                               <option>25</option>
+                               <option>30</option>
+                           </select>
+                           </div>
+                           <div class="shareDiv">
+                           <button className='sharebtn' onClick={myFunction}><Clipboard2Plus/></button>
+                           </div>
+
+
+                           {/* <input type='text' onChange={this.changeRoom.bind(this)}/> */}
+                           <div className='board-container'>
+                               <Board color={color} size={size} room={prams.room}></Board>
+                           </div>
+                       </div>
+
+    )
+    
+}
+
+
+
+export default Container
